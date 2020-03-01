ecabbf924d20b25d7b8c56e87d7691b42dab9c5a
Andrey Shatalov <a.shatalov@rj-games.com>
2020-03-01 20:06:48 +0300

    try to fix linux build

--- src/async.h
+++ src/async.h
@@ -5,12 +5,12 @@
 
 namespace async
 {
+    struct Worker;
     using handle_t = void *;
 
     handle_t connect(std::size_t bulk);
     void receive(handle_t handle, const char* data, std::size_t size);
     void disconnect(handle_t handle);
 
-    struct Worker;
     std::vector<std::shared_ptr<Worker>> _contextCache;
 }
